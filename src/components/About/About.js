cc2f5d19890cbeeb6425a5550263a2b781d086b2
Travis Tincher <travistincher@Traviss-MacBook-Air.local>
2020-12-16 12:18:42 -0600

    grid componient styling

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,23 +5,20 @@ import headshot from "../../assets/img/headshot-transparent.png";
 function About() {
   return (
     <div className="About">
-      <div className="headerBox">
+      <div className="headerBox about_1">
         <h1>About Me</h1>
       </div>
-      <div className="headshot">
+      <div className="headshot about_2">
         <img src={headshot} alt="headshot" />
       </div>
-      <div className="about_body">
-        <p>
-          I’m a <span>Houston based</span> software developer who builds user
-          forward websites with an eye towards integrating function and design!
-        </p>
-        <p>
-          I have a diverse background in philosophy, theology, art and beer,
-          which has given me an eye for aesthetic and a mind for the production
-          cycle.
-        </p>
-      </div>
+      <p className="about_3">
+        I’m a <span>Houston based</span> software developer who builds user
+        forward websites with an eye towards integrating function and design!
+      </p>
+      <p className="about_4">
+        I have a diverse background in philosophy, theology, art and beer, which
+        has given me an eye for aesthetic and a mind for the production cycle.
+      </p>
     </div>
   );
 }
